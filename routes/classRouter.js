2fc2d5da61ec26a75901433106a46f7e8372a21e
JinkyunJ <148345588+JinKyunJ@users.noreply.github.com>
2024-09-11 15:31:42 +0900

    Feat: 모든 클래스 조회, title 검색되는 레슨 조회 추가

--- routes/classRouter.js
+++ routes/classRouter.js
@@ -2,42 +2,56 @@ const { Router } = require("express");
 const asyncHandler = require("../middlewares/async-handler");
 const classService = require("../services/classService");
 // multer 이미지 업로드 설정 가져오기
-const upload = require('../utils/multerConfig');
+const upload = require("../utils/multerConfig");
 
 const router = Router();
 
-/* create  */ 
-router.post('/', upload.array('photo'), asyncHandler(async (req, res) => {
-    const bodyData = req.body;
-    const imageFiles = (!req.files || req.files.length === 0) ? ["notFound"] : req.files;
-    
-  })
+/* create  */
+router.post(
+    "/",
+    upload.array("photo"),
+    asyncHandler(async (req, res) => {
+        const bodyData = req.body;
+        const imageFiles = !req.files || req.files.length === 0 ? ["notFound"] : req.files;
+    }),
 );
 
 /* 전체 레슨 조회 */
 router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    
-  })
+    "/",
+    asyncHandler(async (req, res) => {
+        const result = await classService.findAllClass();
+        return res.status(200).json(result);
+    }),
+);
+
+/* title로 검색되는 레슨 조회 */
+router.get(
+    "/search",
+    asyncHandler(async (req, res) => {
+        const { title } = req.query;
+        const result = await classService.findClassByTitle(title);
+        return res.status(200).json(result);
+    }),
 );
 
 /* nanoid 로 특정 레슨 조회 */
 router.get(
-  "/nanoid",
-  asyncHandler(async (req, res) => {
-    
-  })
+    "/nanoid",
+    asyncHandler(async (req, res) => {}),
 );
 
 // update (수정)
-router.put('/', upload.array('photo'), asyncHandler(async (req, res) => {
-    
-}));
+router.put(
+    "/",
+    upload.array("photo"),
+    asyncHandler(async (req, res) => {}),
+);
 
 // delete (삭제)
-router.delete('/', asyncHandler(async (req, res) => {
-    
-}));
+router.delete(
+    "/",
+    asyncHandler(async (req, res) => {}),
+);
 
 module.exports = router;
--- services/classService.js
+++ services/classService.js
@@ -1,13 +1,38 @@
 const { User, Class } = require("../models");
 const newDate = require("../utils/newDate");
 const crypto = require("crypto");
-const imageToAWS = require('../utils/imageToAWS');
-const deleteImageFromAWS = require('../utils/deleteImageFromAWS');
+const imageToAWS = require("../utils/imageToAWS");
+const deleteImageFromAWS = require("../utils/deleteImageFromAWS");
 
 class ClassService {
     // create
 
     // all class select
+    async findAllClass() {
+        const classes = await Class.find();
+        if (!classes.length) {
+            const error = new Error("조회된 클래스가 없습니다.");
+            Object.assign(error, { code: 404 });
+            throw error;
+        }
+        return { code: 200, data: classes, message: "전체 클래스 조회 완료" };
+    }
+
+    // find class by title
+    async findClassByTitle(title) {
+        if (!title) {
+            const error = new Error("제목이 필요합니다.");
+            Object.assign(error, { code: 400 });
+            throw error;
+        }
+        const titleClass = await Class.find({ title });
+        if (!titleClass.length) {
+            const error = new Error("해당 제목으로 조회된 레슨이 없습니다.");
+            Object.assign(error, { code: 404 });
+            throw error;
+        }
+        return { code: 200, data: titleClass, message: "타이틀로 조회 완료" };
+    }
 
     // one class select
 
