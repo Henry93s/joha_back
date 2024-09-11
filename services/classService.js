75afdd82b86a447872b83f09e0771f6aabf77416
boeun <supbe0726@naver.com>
2024-09-11 14:28:15 +0900

    Feat: 특정 클래스 조회, 삭제 기능 추가

--- services/classService.js
+++ services/classService.js
@@ -1,19 +1,40 @@
 const { User, Class } = require("../models");
 const newDate = require("../utils/newDate");
 const crypto = require("crypto");
-const imageToAWS = require('../utils/imageToAWS');
-const deleteImageFromAWS = require('../utils/deleteImageFromAWS');
+const imageToAWS = require("../utils/imageToAWS");
+const deleteImageFromAWS = require("../utils/deleteImageFromAWS");
 
 class ClassService {
-    // create
+  // create
 
-    // all class select
+  // all class select
 
-    // one class select
+  // one class select
+  async findOneClass(nanoid) {
+    const oneClass = Class.findOne(
+      { nanoid },
+      "nanoid author main_image sub_images title main_available_time sub_available_time main_location sub_location duration_time price contents introduce is_premium like star comments create_at update_at"
+    );
 
-    // update
+    return { data: oneClass, code: 200, message: "클래스 조회 완료" };
+  }
+  // update
 
-    // delete
+  // delete
+  async deleteClass(nanoid) {
+    const oneClass = await Class.findOne({ nanoid });
+    if (!oneClass) {
+      const error = new Error();
+      Object.assign(error, {
+        code: 404,
+        message: "아이디로 조회된 클래스가 없습니다.",
+      });
+      throw error;
+    } else {
+      await Class.deleteOne(oneClass);
+      return { code: 200, message: `${nanoid} 클래스 삭제 동작 완료` };
+    }
+  }
 }
 
 const classService = new ClassService();
